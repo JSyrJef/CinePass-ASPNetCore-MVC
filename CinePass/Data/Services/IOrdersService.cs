c3aa191bb336bd7287a24dd9a13ae40ce7e97678
JeffersonS <perezsanchezjeffersonheriberto@gmail.com>
2023-05-14 00:10:08 -0600

    Fix Controller Order and Fix Service Order

--- CinePass/Data/Services/IOrdersService.cs
+++ CinePass/Data/Services/IOrdersService.cs
@@ -1,6 +1,12 @@
-﻿namespace CinePass.Data.Services
+﻿using CinePass.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CinePass.Data.Services
 {
     public interface IOrdersService
     {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole);
     }
 }
