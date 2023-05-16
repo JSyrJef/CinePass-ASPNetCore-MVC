dcb8a6244036cb07804754dc68c578f97b37950b
JeffersonS <perezsanchezjeffersonheriberto@gmail.com>
2023-05-16 14:55:09 -0600

    Fixed Shared folder and site.css file

--- CinePass/Startup.cs
+++ CinePass/Startup.cs
@@ -31,6 +31,7 @@ namespace CinePass
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             //DbContext configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
 
